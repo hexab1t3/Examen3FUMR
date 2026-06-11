5f066024d6d771aa8663fae6c38904b5a85b7c4a
hexab1t3 <fer.uri.mi.ro@gmail.com>
2026-06-10 20:32:00 -0600

    Validación de credenciales

--- app/src/main/java/com/example/examen3/MainActivity.kt
+++ app/src/main/java/com/example/examen3/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.examen3
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +25,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
         val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+
+        btnIniciarSesion.setOnClickListener {
+            val nombre = etNombre.text.toString()
+            val password = etPassword.text.toString()
+
+            val db = MyApplication.getDatabase(this)
+            lifecycleScope.launch(Dispatchers.IO) {
+                val usuario = db.usuarioDao().buscarUsuario(nombre, password)
+                withContext(Dispatchers.Main) {
+                    if (usuario != null) {
+                        val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this@MainActivity, "Las credenciales no son correctas", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
         btnRegistrarse.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
