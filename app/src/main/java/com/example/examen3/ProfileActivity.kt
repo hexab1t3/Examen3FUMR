3728bb8f026ee237510b3e41d1766dee6d43536d
hexab1t3 <fer.uri.mi.ro@gmail.com>
2026-06-10 20:32:23 -0600

    Almacenamiento de última conexión

--- app/src/main/java/com/example/examen3/ProfileActivity.kt
+++ app/src/main/java/com/example/examen3/ProfileActivity.kt
@@ -1,10 +1,14 @@
 package com.example.examen3
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,17 @@ class ProfileActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvUltimaConexion = findViewById<TextView>(R.id.tvUltimaConexion)
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+
+        // ultima conexión guardada
+        val ultimaConexion = prefs.getString("ultima_conexion", "Sin registro")
+        tvUltimaConexion.text = "Última conexión: $ultimaConexion"
+
+        // Guardar la fecha y hora actual como ultima conexion
+        val formato = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+        val fechaActual = formato.format(Date())
+        prefs.edit().putString("ultima_conexion", fechaActual).apply()
     }
 }
